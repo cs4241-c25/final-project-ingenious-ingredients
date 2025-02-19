9be2af7edc7de7ae885f51db5194a1fb70b05a5c
zoefisk <94054962+zoefisk@users.noreply.github.com>
2025-02-19 11:41:36 -0500

    added parallax effect to hero page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      backgroundImage: {
+        'parallax' : 'url("https://images.unsplash.com/photo-1488992783499-418eb1f62d08?q=80&w=3389&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+      }
     },
   },
   plugins: [],
