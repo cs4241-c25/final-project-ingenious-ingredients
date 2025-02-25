aa4cb65321b9578eea422d0ab7b9b094cf77fb81
zoefisk <94054962+zoefisk@users.noreply.github.com>
2025-02-25 16:16:32 -0500

    slight update to recipe card

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -5,44 +5,46 @@ import CardContent from '@mui/joy/CardContent';
 import CardOverflow from '@mui/joy/CardOverflow';
 import Divider from '@mui/joy/Divider';
 import Typography from '@mui/joy/Typography';
+import '../app/globals.css';
 
-export default function OverflowCard() {
+export default function RecipeCard() {
     return (
-        <Card variant="outlined" sx={{ width: 320 }} size="lg">
-            <CardOverflow>
-                <AspectRatio ratio="2">
-                    <img
-                        src="https://images.unsplash.com/photo-1532614338840-ab30cf10ed36?auto=format&fit=crop&w=318"
-                        srcSet="https://images.unsplash.com/photo-1532614338840-ab30cf10ed36?auto=format&fit=crop&w=318&dpr=2 2x"
-                        loading="lazy"
-                        alt=""
-                    />
-                </AspectRatio>
-            </CardOverflow>
-            <CardContent>
-                <Typography level="title-md">Yosemite National Park</Typography>
-                <Typography level="body-sm">California</Typography>
-            </CardContent>
-            <CardOverflow variant="soft" sx={{ bgcolor: 'background.level1' }}>
-                <Divider inset="context" />
-                <CardContent orientation="horizontal">
-                    <Typography
-                        level="body-xs"
-                        textColor="text.secondary"
-                        sx={{ fontWeight: 'md' }}
-                    >
-                        6.3k views
-                    </Typography>
-                    <Divider orientation="vertical" />
-                    <Typography
-                        level="body-xs"
-                        textColor="text.secondary"
-                        sx={{ fontWeight: 'md' }}
-                    >
-                        1 hour ago
-                    </Typography>
+        <a href="https://example.com" className="recipe-card-link">
+            <Card variant="outlined" sx={{width: 320}} size="lg">
+                <CardOverflow>
+                    <AspectRatio ratio="2">
+                        <img
+                            srcSet="https://images.unsplash.com/photo-1532614338840-ab30cf10ed36?auto=format&fit=crop&w=318&dpr=2 2x"
+                            loading="lazy"
+                            alt=""
+                        />
+                    </AspectRatio>
+                </CardOverflow>
+                <CardContent>
+                    <Typography level="title-md">Yosemite National Park</Typography>
+                    <Typography level="body-sm">California</Typography>
+                    <CardOverflow variant="soft" sx={{bgcolor: 'background.level1'}}>
+                        <Divider inset="context"/>
+                        <CardContent orientation="horizontal">
+                            <Typography
+                                level="body-xs"
+                                textColor="text.secondary"
+                                sx={{fontWeight: 'md'}}
+                            >
+                                6.3k views
+                            </Typography>
+                            <Divider orientation="vertical"/>
+                            <Typography
+                                level="body-xs"
+                                textColor="text.secondary"
+                                sx={{fontWeight: 'md'}}
+                            >
+                                1 hour ago
+                            </Typography>
+                        </CardContent>
+                    </CardOverflow>
                 </CardContent>
-            </CardOverflow>
-        </Card>
-    );
+            </Card>
+        </a>
+);
 }
