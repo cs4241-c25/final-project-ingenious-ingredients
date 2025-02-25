3ee487f591d4f6f008c5e2ba6f6ee27b494ebfd3
zoefisk <94054962+zoefisk@users.noreply.github.com>
2025-02-25 18:59:16 -0500

    Renamed function name

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import RecipeCard from "@/components/RecipeCard";
 import RecipeGrid from "@/components/RecipeGrid";
 
-
-const Page = () => {
+export default function Browse() {
     return (
         <div>
             <nav id={'replace w navbar'}/>
@@ -13,5 +12,3 @@ const Page = () => {
 
     );
 }
-
-export default Page;
