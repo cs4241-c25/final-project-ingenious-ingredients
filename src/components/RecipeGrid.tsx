8611b81ef7091a86c72ab4537ee75d168954ead9
zoefisk <94054962+zoefisk@users.noreply.github.com>
2025-02-25 19:18:43 -0500

    added sample data to recipe grid

--- src/components/RecipeGrid.tsx
+++ src/components/RecipeGrid.tsx
@@ -15,7 +15,7 @@ const RecipeGrid: React.FC<RecipeGridProps> = ({colNum}) => {
         <Grid2 container spacing={3} columns={colNum} sx={{ width: '100%', margin: "auto", padding: 2, justifyContent: "center"}}>
             {items.map((item, index) => (
                 <Grid2 sx={{xs: 6, sm: 4, md: 3, justifyContent: "center"}} key={index} component="div">
-                    <RecipeCard/>
+                    <RecipeCard name={"Cheeseburger"} creator={"Andrew"} prepTime={"30 minutes"} image={"https://upload.wikimedia.org/wikipedia/commons/4/4d/Cheeseburger.jpg"} likes={23} postDate={"February 1, 2025"} ingredients={["beef", "cheese", "lettuce"]} tags={["delicious", "yummy", "cool"]}/>
                 </Grid2>
             ))}
         </Grid2>
