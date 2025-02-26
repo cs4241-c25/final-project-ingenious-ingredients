a95756a97eef2aad122c66a15b0cebf0542a5b56
zoefisk <94054962+zoefisk@users.noreply.github.com>
2025-02-25 21:54:54 -0500

    changed recipe grid to be dynamically usable

--- src/components/RecipeGrid.tsx
+++ src/components/RecipeGrid.tsx
@@ -7,32 +7,31 @@ import {Step} from "../../Classes/Step";
 import {RecipeIngredient} from "../../Classes/RecipeIngredient";
 
 interface RecipeGridProps {
-    colNum: number
+    colNum: number,
+    recipes: Recipe[],
+    maxRecipes?: number,
 }
 
-// TODO: make this component take in number of recipes to display (optional), this would be in case on the heros page for example it wouldnt list all the recipes on the site.
+const RecipeGrid: React.FC<RecipeGridProps> = ({recipes, colNum, maxRecipes}) => {
 
-const RecipeGrid: React.FC<RecipeGridProps> = ({colNum}) => {
+    // let ingredient1 = new RecipeIngredient("beef", 1, "lb");
+    // let ingredient2 = new RecipeIngredient("cheese", 1, "slice");
+    // let ingredient3 = new RecipeIngredient("lettuce", 1, "leaf");
+    // let step1 = new Step("Cook the beef", [ingredient1]);
+    // let step2 = new Step("Put the cheese on the beef", [ingredient1, ingredient2]);
+    // let step3 = new Step("Put the lettuce on the cheese", [ingredient3]);
+    //
+    // let Cheeseburger = new Recipe([step1, step2, step3], "Cheeseburger", "Andrew", true, 30, "lunch", 23, "February 1, 2025", [ingredient1, ingredient2, ingredient3], ["delicious", "yummy", "cool"]);
+    // let Pizza = new Recipe([step1, step2, step3], "Pizza", "John", true, 20, "dinner", 50, "March 1, 2025", [ingredient1, ingredient2, ingredient3], ["tasty", "cheesy"]);
+    // let Salad = new Recipe([step1, step2, step3], "Salad", "Jane", true, 10, "lunch", 15, "April 1, 2025", [ingredient1, ingredient2, ingredient3], ["healthy", "fresh"]);
+    // const recipes = [Cheeseburger, Pizza, Salad];
 
-    let ingredient1 = new RecipeIngredient("beef", 1, "lb");
-    let ingredient2 = new RecipeIngredient("cheese", 1, "slice");
-    let ingredient3 = new RecipeIngredient("lettuce", 1, "leaf");
-    let step1 = new Step("Cook the beef", [ingredient1]);
-    let step2 = new Step("Put the cheese on the beef", [ingredient1, ingredient2]);
-    let step3 = new Step("Put the lettuce on the cheese", [ingredient3]);
-
-    let Cheeseburger = new Recipe([step1, step2, step3], "Cheeseburger", "Andrew", true, 30, "lunch", 23, "February 1, 2025", [ingredient1, ingredient2, ingredient3], ["delicious", "yummy", "cool"]);
-    let Pizza = new Recipe([step1, step2, step3], "Pizza", "John", true, 20, "dinner", 50, "March 1, 2025", [ingredient1, ingredient2, ingredient3], ["tasty", "cheesy"]);
-    let Salad = new Recipe([step1, step2, step3], "Salad", "Jane", true, 10, "lunch", 15, "April 1, 2025", [ingredient1, ingredient2, ingredient3], ["healthy", "fresh"]);
-
-    const recipes = [Cheeseburger, Pizza, Salad];
-
-    function getTopRecipes(recipes: Recipe[]) {
-        return recipes
-            .sort((a, b) => b.likes - a.likes)
-            // .slice(0, 10);         // Trending recipes might only be the top 10 recipes
+    function getTopRecipes(recipes) {
+        if (!maxRecipes) {
+            return recipes.sort((a, b) => b.likes - a.likes);
+        }
+        return recipes.sort((a, b) => b.likes - a.likes).slice(0, maxRecipes);
     }
-
     const topRecipes = getTopRecipes(recipes);
 
     return (
