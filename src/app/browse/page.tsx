11169cb65f1a8bb4c20fbb2aa9712657192ba3f6
zoefisk <94054962+zoefisk@users.noreply.github.com>
2025-02-25 21:56:12 -0500

    started filling out browse recipes page

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import RecipeCard from "@/components/RecipeCard";
+"use client";
+
+import React, {useEffect, useState} from 'react';
 import RecipeGrid from "@/components/RecipeGrid";
+import {GetAllRecipes} from "@/Get-Post Requests/Recipe/getAllRecipes";
+import {Recipe} from "../../../Classes/Recipe";
 
 export default function Browse() {
+    const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+    useEffect(() => {
+        async function fetchRecipes() {
+            const allRecipes = await GetAllRecipes();
+            setRecipes(allRecipes);
+        }
+        fetchRecipes();
+    }, []);
+
     return (
         <div>
-            <nav id={'replace w navbar'}/>
-
-
+            <RecipeGrid colNum={3} recipes={recipes} />
         </div>
-
     );
 }
