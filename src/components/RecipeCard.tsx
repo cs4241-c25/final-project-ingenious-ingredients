79512f0fd588cfe2cb4906cec655db96446ccd45
zoefisk <94054962+zoefisk@users.noreply.github.com>
2025-02-25 22:26:35 -0500

    Update RecipeCard.tsx

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -34,7 +34,7 @@ export default function RecipeCard({ recipe, image }: RecipeCardProps) {
     }
 
     return (
-        <Link href={`/browse/${slug}`}>
+        <Link href={`src/app/browse/${recipe.slug}`}>
             <Card variant="outlined" sx={{width: 320}} size="lg">
                 <CardOverflow>
                     <AspectRatio ratio="2">
