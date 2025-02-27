112362c122968c0be39c14bdbf1d015b5016ab24
zoefisk <94054962+zoefisk@users.noreply.github.com>
2025-02-27 15:16:25 -0500

    Revert "Add Zoe's changes into main"

--- src/Get-Post Requests/Recipe/getRecipeFromSlug.ts
+++ src/Get-Post Requests/Recipe/getRecipeFromSlug.ts
@@ -1,25 +1,19 @@
-import { Recipe } from "../../../Classes/Recipe";
+import {Recipe} from "@/Classes/Recipe";
 
-export async function GetRecipeFromSlug(slug: string): Promise<Recipe | null> {
-    const json = { slug: slug };
+export async function GetRecipeFromSlug(slug: string): Promise<Recipe> {
+    const json = {
+        slug: slug
+    }
+
     const body = JSON.stringify(json);
 
     const results = await fetch('http://localhost:3000/getRecipeFromSlug', {
         method: 'POST',
         body,
-        headers: { "Content-Type": "application/json" }
-    });
-
-    if (!results.ok) {
-        console.error(`Error fetching recipe: ${results.statusText}`);
-        return null;
-    }
+        headers: {"Content-Type": "application/json"}
+    })
 
     const recipe = await results.json();
-    if (recipe.error) {
-        console.error(recipe.error);
-        return null;
-    }
 
     return new Recipe (
         recipe.steps,
