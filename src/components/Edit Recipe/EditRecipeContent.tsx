ac4481b85205f2bf5d971d2b84383003389935cb
zoefisk <94054962+zoefisk@users.noreply.github.com>
2025-03-04 14:26:35 -0500

    fixed formatting

--- src/components/Edit Recipe/EditRecipeContent.tsx
+++ src/components/Edit Recipe/EditRecipeContent.tsx
@@ -24,14 +24,12 @@ export default function EditRecipeContent({ recipe }: EditRecipeContentProps) {
     return (
         <>
             <TextField/>
-
             <div>
                 <Button onClick={handleDeleteButton}
                         className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
                     Delete Recipe
                 </Button>
             </div>
-
-            </>
-            );
-            }
+        </>
+    );
+}
