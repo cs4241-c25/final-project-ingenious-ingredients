9557b62db865f9d12dd42d7ea38cb8c22a4f3ee1
zoefisk <94054962+zoefisk@users.noreply.github.com>
2025-03-05 11:18:54 -0500

    updated the like button to include unlike functionality using UnlikeRecipe() endpoint

--- src/components/LikeRecipeButton.tsx
+++ src/components/LikeRecipeButton.tsx
@@ -1,9 +1,11 @@
 import { LikeRecipe } from "@/Get-Post Requests/Recipe/likeRecipe";
+import { UnlikeRecipe } from "@/Get-Post Requests/Recipe/unlikeRecipe";
 import Button from "@mui/material/Button";
 import RecommendIcon from '@mui/icons-material/Recommend';
 import { Session } from "next-auth";
 import { Recipe } from "../../Classes/Recipe";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import {GetUser} from "@/Get-Post Requests/User/getUser";
 
 interface LikeRecipeButtonProps {
     recipe: Recipe;
@@ -13,21 +15,41 @@ interface LikeRecipeButtonProps {
 
 export default function LikeRecipeButton({ recipe, session, onClick }: LikeRecipeButtonProps) {
     const [likes, setLikes] = useState(recipe.likes);
+    const [liked, setLiked] = useState(false);
+
+    useEffect(() => {
+        // Check if the user has already liked the recipe
+        if (session && session.user) {
+            // Assume we have a function to get the user data
+            const fetchUser = async () => {
+                const user = await GetUser(session.user.name);
+                const userHasLiked = user.favoritedRecipes.includes(recipe.slug);
+                setLiked(userHasLiked);
+            };
+            fetchUser();
+        }
+    }, [session, recipe.slug]);
 
     async function handleLikeRecipe() {
         if (!session || !session.user) {
             return alert("You must be logged in to like a recipe");
         }
 
-        // TODO: If user has already liked this recipe, unlike it. Tommy will make this endpoint soon.
-        // TODO: If user has already liked this recipe, grey out the button or change color or something.
-
-        await LikeRecipe(session.user.name, recipe.slug);
-        setLikes(likes + 1); // Update the likes state to trigger a re-render
+        if (liked) {
+            await UnlikeRecipe(session.user.name, recipe.slug);
+            setLikes(likes - 1);
+        } else {
+            await LikeRecipe(session.user.name, recipe.slug);
+            setLikes(likes + 1);
+        }
+        setLiked(!liked);
     }
 
     return (
-        <Button onClick={(e) => { onClick?.(e); handleLikeRecipe(); }}>
+        <Button
+            onClick={(e) => { onClick?.(e); handleLikeRecipe(); }}
+            style={{ color: liked ? 'grey' : 'inherit' }}
+        >
             <RecommendIcon /> {likes}
         </Button>
     );
